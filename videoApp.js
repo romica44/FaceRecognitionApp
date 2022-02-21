6107557822a19a116c55b9f5496984761f26a082
unknown <romica44@hotmail.com>
2022-02-20 23:33:49 -0300

    commit

--- videoApp.js
+++ videoApp.js
@@ -1,19 +1,16 @@
+function importarScript(nombre) {
+    var s = document.createElement("script");
+    s.src = nombre;
+    document.querySelector("head").appendChild(s);
+}
+importarScript("face-api.min.js")
+
 
 
 const video= document.getElementById('video');//llamo a la etiqueta video del index.html
 
-Promise.all([
-    
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-    faceapi.nets.ageGenderNet.loadFromUri('/models')
-]).then(startVideo);
-
 function startVideo(){
-    
-    navigator.getUserMedia(// navigator.mediaDevices.getUserMedia
+    navigator.getUserMedia(//navigator.mediaDevices.getUserMedia
         {video: {} },
         stream => video.srcObject = stream,
         err => console.log(err)
@@ -21,16 +18,24 @@ function startVideo(){
 }
 startVideo()
 
+Promise.all([
+    
+    faceapi.nets.tinyFaceDetector.loadFromUri("https://drive.google.com/drive/folders/1nWolNW0f7g_pheA0TQZUWCFg-u0sQrUj?usp=sharing"),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/models'),
+]).then(startVideo);
 
 
-video.addEventListener('play', () => { //capturar el evento play sobre el objeto video
+video.addEventListener('play', () => { //caputar el evento play sobre el objeto video
  const canvas = faceapi.createCanvasFromMedia(video);
  document.body.append(canvas);
  const displaySize = {width: video.width, height: video.height};
  faceapi.matchDimensions(canvas,displaySize);
  setInterval (async ()=>{
      const detections = await faceapi.detectAllFaces(video, new 
-        faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        faceapi.tinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
      console.log(detections)
 
      const resizeDetections = faceapi.resizeResults(detections, displaySize);
@@ -40,5 +45,5 @@ video.addEventListener('play', () => { //capturar el evento play sobre el objeto
      faceapi.draw.drawDetections(canvas, resizeDetections);
      faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
      faceapi.draw.drawFaceExpressions(canvas, resizeDetections);
- }, 200)
+ }, 100)
 });
